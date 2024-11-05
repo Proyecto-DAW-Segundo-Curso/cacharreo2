8262ef04e534cab3204a4a17e49fc1943b405648
Gonzalosg <gonzaloms26@gmail.com>
2024-11-05 13:15:09 +0100

    componente al main

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,10 @@
 <script>
   import Header from "./lib/Header.svelte";
   import Contador from "./lib/Contador.svelte";
-    import ModoVisual from "./lib/ModoVisual.svelte";
-    import LineAdicional from "./lib/componentes/LineaAdicional.svelte";
+  import ModoVisual from "./lib/ModoVisual.svelte";
+  import LineAdicional from "./lib/LineaAdicional.svelte";
   import Boton from "./lib/boton.svelte";
+    import Darkmode from "./lib/Darkmode.svelte";
 
   let colores = ["blue", "red", "green", "orange"];
 </script>
@@ -23,5 +24,6 @@
 <ModoVisual />
 <LineAdicional />
 <Boton />
+<Darkmode/>
 <style>
 </style>
