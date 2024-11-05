8fbafbd096fadd30850ea1153a06b11e49840df1
Gonzalosg <gonzaloms26@gmail.com>
2024-11-05 12:39:54 +0100

    componenteDarkMode

---
+++ src/lib/Darkmode.svelte
@@ -0,0 +1,40 @@
+<script>
+
+    let isDarkMode=false;
+    function cambiarModo(){
+        isDarkMode = !isDarkMode;
+    }
+
+</script>
+
+<div class="container" class:dark-mode={isDarkMode} class:light-mode={!isDarkMode}>
+
+    <button on:click={cambiarModo}>
+        Cambiar a {isDarkMode ? 'Modo Oscuro' : 'Modo Claro'}
+    </button>
+
+    <p>dfgdgdfghfhgyjyhjyjyjyjhnghgfghfgh</p>
+    <p>dfgdgdfghfhgyjyhjyjyjyjhnghgfghfgh</p>
+    <p>dfgdgdfghfhgyjyhjyjyjyjhnghgfghfgh</p>
+    <p>dfgdgdfghfhgyjyhjyjyjyjhnghgfghfgh</p>
+
+</div>
+
+
+<style>
+
+   
+
+    .dark-mode{
+        background-color: aliceblue;
+        color: black;
+        
+    }
+
+    .light-mode{
+        background-color: black;
+        color: white;   
+        
+    }
+
+</style>
